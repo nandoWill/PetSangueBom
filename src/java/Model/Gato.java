653ce1bdd5eb2f2fe786b6af9efd0b212d404738
iUlisses <nero.tommo@gmail.com>
2017-07-08 23:49:02 -0300

    ei
    baixa sabuceta aí e seja feliz eu vou salvar minha casa.

--- src/java/Model/Gato.java
+++ src/java/Model/Gato.java
@@ -5,10 +5,33 @@
  */
 package Model;
 
+import java.util.Date;
+
 /**
  *
  * @author Nêro
  */
-public class Gato {
+public class Gato extends Animal implements AnimalFactory {
+    private Date ultimaDoacao;
+    public Gato(String nome, float peso, String raca, String tipoSanguineo, int idAnimal, Date ultimaDoacao) {
+        super(nome, peso, raca, tipoSanguineo, idAnimal);
+        this.ultimaDoacao = ultimaDoacao;
+    }
+
+    private boolean isDoador() {
+        //precisa ser feita a verificação da última vez que ele doou pra ver se ele vai poder ser um doador. 
+        return false;
+    }
+
+    @Override
+    //precisa ser criada as exceções caso o animal não seja apto a doar jogarmos a doação de prontidão.
+    public boolean criarAnimal() {
+       
+        if(isDoador() == true){
+        // se as regras de comparação forem verdade o retorno confirmara que ele está apto a doar.
+        }
+        
+     return true;   
+    }
     
 }
