cc636d2988e97969e3545ac6405dde3e78292ac5
nando <bolodecenoura@alu.ufc.br>
2017-07-08 22:25:11 -0300

    EnderecoDAO Base
    fiz o começo da implementação do buscar endereco

---
+++ src/java/PacoteDAO/EnderecoDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PacoteDAO;
+import Model.Endereco;
+
+/**
+ *
+ * @author biscoito
+ */
+public class EnderecoDao {
+   Endereco enderecoDao = new endereco();
+}
