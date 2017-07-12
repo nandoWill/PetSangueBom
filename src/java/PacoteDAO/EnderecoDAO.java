c374ee0c0c2e16448a1f44b598b197d4d0bc1b8c
nando <bolodecenoura@alu.ufc.br>
2017-07-11 23:22:33 -0300

    DAO Criadas
    Welcome seus puto by pato papão

---
+++ src/java/PacoteDAO/EnderecoDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PacoteDAO;
+import Model.Endereco;
+
+/**
+ *
+ * @author biscoito
+ */
+public class EnderecoDao {
+   Endereco enderecoDao = new Endereco();
+   
+   public String buscarEndereco(String endereco){
+       String resultado = endereco;
+       
+       return resultado;
+   }
+}
